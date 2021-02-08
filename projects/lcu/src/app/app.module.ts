be007cb186570d29b3fdcccb36b2ee6595adcbdc
mcgear <michael.gearhardt@gmail.com>
2021-02-08 14:35:08 -0700

    init lcu

---
+++ projects/lcu/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FathymSharedModule, LCUServiceSettings } from '@lcu/common';
+import { environment } from '../environments/environment';
+import { LandingPagesModule } from '@lowcodeunit/landing-pages-common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FathymSharedModule,
+    LandingPagesModule.forRoot()
+  ],
+  providers: [
+    {
+      provide: LCUServiceSettings,
+      useValue: FathymSharedModule.DefaultServiceSettings(environment)
+    }
+  ],
+  exports: [LandingPagesModule]
+})
+export class AppModule {}
