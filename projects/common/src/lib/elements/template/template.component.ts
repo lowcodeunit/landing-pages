4a3e0ec98f1c847d470de5522eaed1e839785256
xchanin <shannonwbruns@gmail.com>
2021-02-09 07:41:25 -0700

    AB#NoStoryNum export models

--- projects/common/src/lib/elements/template/template.component.ts
+++ projects/common/src/lib/elements/template/template.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { TemplateConfigModel } from './../../models/template-config.model';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lcu-template',
@@ -7,8 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  /**
+   * Template configuration
+   */
+  private _config: TemplateConfigModel;
+  @Input('config')
+  set Config(val: TemplateConfigModel) {
+    if (!val) {
+      return;
+    }
 
-  
+    this._config = val;
+
+  }
+  get Config(): TemplateConfigModel {
+
+    if (!this._config) {
+      return;
+    }
+    return this._config;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
