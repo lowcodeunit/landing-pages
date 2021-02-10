8303493c2968e7cb541bba8abe7a861bb5aaea49
xchanin <shannonwbruns@gmail.com>
2021-02-10 12:47:06 -0700

    AB#9325 upgrade to Angular 11

--- projects/blocks/src/app/app-routing.module.ts
+++ projects/blocks/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesModule } from './pages/pages.module';
 
 const routes: Routes = [
   {
@@ -10,7 +11,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
   ],
   exports: [RouterModule]
 })
