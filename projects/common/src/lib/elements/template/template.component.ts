36734087219fef73e6659308503e842328e68e82
mcgear <michael.gearhardt@gmail.com>
2021-02-11 16:54:06 -0700

    Upgraded lcu/common

--- projects/common/src/lib/elements/template/template.component.ts
+++ projects/common/src/lib/elements/template/template.component.ts
@@ -6,39 +6,22 @@ export class LandingPagesTemplateElementState {}
 
 export class LandingPagesTemplateContext extends LCUElementContext<LandingPagesTemplateElementState> {}
 
-export const SELECTOR_LANDING_PAGES_TEMPLATE_ELEMENT = 'lcu-landing-pages-template-element';
+export const SELECTOR_LANDING_PAGES_TEMPLATE_ELEMENT =
+  'lcu-landing-pages-template-element';
 
 @Component({
   selector: SELECTOR_LANDING_PAGES_TEMPLATE_ELEMENT,
   templateUrl: './template.component.html',
-  styleUrls: ['./template.component.scss']
+  styleUrls: ['./template.component.scss'],
 })
-export class LandingPagesTemplateElementComponent extends LcuElementComponent<LandingPagesTemplateContext> implements OnInit {
+export class LandingPagesTemplateElementComponent
+  extends LcuElementComponent<LandingPagesTemplateContext>
+  implements OnInit {
   //  Fields
-  /**
-   * Template configuration
-   */
-  protected _config: LandingPageConfig;
 
   //  Properties
   @Input('config')
-  public set Config(val: LandingPageConfig) {
-    if (!val) {
-      return;
-    }
-
-    this._config = val;
-
-  }
-
-  public get Config(): LandingPageConfig {
-
-    if (!this._config) {
-      return;
-    }
-    return this._config;
-  }
-
+  public Config: LandingPageConfig;
 
   //  Constructors
   constructor(protected injector: Injector) {
