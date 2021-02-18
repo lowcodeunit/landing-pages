c7687858c901e220e75e32160d5b8f492149d9c9
mcgear <michael.gearhardt@gmail.com>
2021-02-18 11:19:06 -0700

    Change where registering things

--- projects/demo/src/app/pages/blocks/blocks.component.ts
+++ projects/demo/src/app/pages/blocks/blocks.component.ts
@@ -17,40 +17,40 @@ export class BlocksComponent implements OnInit {
 
   //  Life Cycle
   public ngOnInit(): void {
-    // this.BlocksLandingPageContext = {
-    //   CallToAction: {
-    //     Action: {
-    //       Text: 'Sign up today',
-    //       Target: '_blank',
-    //       Path: '#',
-    //       Raised: true,
-    //       Color: 'primary'
-    //     },
-    //     Message:
-    //       'To learn how fathym can save your brewery thousands of dollars in time and lost product.',
-    //   },
-    //   Hero: {
-    //     BackgroundImage: 'assets/images/daniel-vogel-sVothhm7iRI-unsplash.jpg',
-    //     Message:
-    //       'Helps breweries develop solutions that can add a significant level of insight and reliability to brewing',
-    //     SubTitle: 'for Breweries',
-    //     Title: 'IoT Ensemble',
-    //   },
-    //   InfoCards: [
-    //     {
-    //       Icon: 'backup',
-    //       Info:
-    //         'Connect sensors to IoT Ensemble and collect real-time data in the cloud.',
-    //       Title: 'Connect Sensors',
-    //     },
-    //     {
-    //       Icon: 'insert_chart',
-    //       Info:
-    //         'Easily access data for use in 3rd party dashboards and reports.',
-    //       Title: 'Access Data',
-    //     },
-    //   ],
-    // };
+    this.BlocksLandingPageContext = {
+      CallToAction: {
+        Action: {
+          Text: 'Sign up today',
+          Target: '_blank',
+          Path: '#',
+          Raised: true,
+          Color: 'primary',
+        },
+        Message:
+          'To learn how fathym can save your brewery thousands of dollars in time and lost product.',
+      },
+      Hero: {
+        BackgroundImage: 'assets/images/daniel-vogel-sVothhm7iRI-unsplash.jpg',
+        Message:
+          'Helps breweries develop solutions that can add a significant level of insight and reliability to brewing',
+        SubTitle: 'for Breweries',
+        Title: 'IoT Ensemble',
+      },
+      InfoCards: [
+        {
+          Icon: 'backup',
+          Info:
+            'Connect sensors to IoT Ensemble and collect real-time data in the cloud.',
+          Title: 'Connect Sensors',
+        },
+        {
+          Icon: 'insert_chart',
+          Info:
+            'Easily access data for use in 3rd party dashboards and reports.',
+          Title: 'Access Data',
+        },
+      ],
+    };
   }
 
   //  API Methods
