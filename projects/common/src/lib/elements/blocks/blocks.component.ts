4d503cbe3b3a9fe66892f40d06fb2e7b9380a08c
mcgear <michael.gearhardt@gmail.com>
2021-02-18 11:17:26 -0700

    Checkpoint

--- projects/common/src/lib/elements/blocks/blocks.component.ts
+++ projects/common/src/lib/elements/blocks/blocks.component.ts
@@ -38,6 +38,8 @@ export class LandingPagesBlocksElementComponent
 
   //  Helpers
   protected setContext(): void {
+    console.log(this.settings.State);
+
     this.SetContext(this.settings.State.LandingPage);
   }
 }
