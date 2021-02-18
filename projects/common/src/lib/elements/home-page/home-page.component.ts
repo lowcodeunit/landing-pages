313a27c22fd608aa24f19a50a51785dfa1fe5a6a
mcgear <michael.gearhardt@gmail.com>
2021-02-18 09:33:30 -0700

    Adjusted home-page format

--- projects/common/src/lib/elements/home-page/home-page.component.ts
+++ projects/common/src/lib/elements/home-page/home-page.component.ts
@@ -1,30 +1,47 @@
 import { Component, OnInit, Injector, HostBinding } from '@angular/core';
-import { LCUElementContext, LcuElementComponent } from '@lcu/common';
+import {
+  LCUElementContext,
+  LcuElementComponent,
+  LCUServiceSettings,
+} from '@lcu/common';
 import { LandingPagesHomePageContext } from './home-page.models';
 
-export const SELECTOR_LANDING_PAGES_HOME_PAGE_ELEMENT = 'landing-pages-home-page-element';
+export const SELECTOR_LANDING_PAGES_HOME_PAGE_ELEMENT =
+  'landing-pages-home-page-element';
 
 @Component({
   selector: SELECTOR_LANDING_PAGES_HOME_PAGE_ELEMENT,
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
-export class LandingPagesHomePageElementComponent extends LcuElementComponent<LandingPagesHomePageContext> implements OnInit {
+export class LandingPagesHomePageElementComponent
+  extends LcuElementComponent<LandingPagesHomePageContext>
+  implements OnInit {
   //  Fields
 
   //  Properties
 
   //  Constructors
-  constructor(protected injector: Injector) {
+  constructor(
+    protected injector: Injector,
+    protected settings: LCUServiceSettings
+  ) {
     super(injector);
   }
 
   //  Life Cycle
   public ngOnInit() {
     super.ngOnInit();
+
+    if (!this.Context) {
+      this.setContext();
+    }
   }
 
   //  API Methods
 
   //  Helpers
+  protected setContext(): void {
+    this.Context = this.settings.State.LandingPage;
+  }
 }
